359f8aacb29ab28c302ab952b5406ca0a3d62f7d
Felipe Marini <felipemarini71188@gmail.com>
2021-02-03 22:33:33 -0300

    Unindo o front com o back

--- InstaDev_MVC/Controllers/FeedController.cs
+++ InstaDev_MVC/Controllers/FeedController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using InstaDev_MVC.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -15,7 +16,6 @@ namespace InstaDev_MVC.Controllers
 
         
         
-        [Route("Publicar")]
         public IActionResult Index()
         {
             ViewBag.POSTS = Post.ReadAll();
@@ -23,6 +23,9 @@ namespace InstaDev_MVC.Controllers
         }
         
         
+        
+        
+        [Route("Publicar")]
         public IActionResult Postar(IFormCollection form)
         {
             
@@ -30,17 +33,50 @@ namespace InstaDev_MVC.Controllers
             post.IdPublicacao = Int32.Parse(form["IdPublicacao"]);
             post.IdUsuario = Int32.Parse(form["IdUsuario"]);
             post.Legenda = form["Legenda"];
-            post.Imagem = form["Imagem"];
+            
+            
+            if(form.Files.Count > 0)
+            {
+                
+                var file    = form.Files[0];
+                var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img_publicacao");
+
+                if(!Directory.Exists(folder)){
+                    
+                    Directory.CreateDirectory(folder);
+                }
+                
+        
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img_publicacao", folder, file.FileName);
+                
+                
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                
+                post.Imagem   = file.FileName;                
+            
+            }
+            
+            else
+            {
+                post.Imagem   = "padrao.png";
+            }
+            
+            
+            
             post.Likes = Int32.Parse(form["IdPublicacao"]);
             
             Post.Create(post);            
+            
             ViewBag.POSTS = Post.ReadAll();
 
-            return LocalRedirect("Publicar");
+            return LocalRedirect("~Feed/Publicar");
         }
         
         
-        [Route("Comentar")]
+        // [Route("Comentar")]
 
         // public IActionResult Index2()
         // {
@@ -50,18 +86,18 @@ namespace InstaDev_MVC.Controllers
         
         
         
-        public IActionResult Comentar(IFormCollection form)
-        {
-            Comentario comment = new Comentario();
-            comment.IdUsuario = Int32.Parse(form["IdUsuario"]);
-            comment.IdComentario = Int32.Parse(form["IdComentario"]);
-            comment.Mensagem = form["Mensagem"];
+        // public IActionResult Comentar(IFormCollection form)
+        // {
+        //     Comentario comment = new Comentario();
+        //     comment.IdUsuario = Int32.Parse(form["IdUsuario"]);
+        //     comment.IdComentario = Int32.Parse(form["IdComentario"]);
+        //     comment.Mensagem = form["Mensagem"];
             
-            Comment.CriarComentario(comment);            
-            ViewBag.COMMENTS = Comment.ListarComentarios();
+        //     Comment.CriarComentario(comment);            
+        //     ViewBag.COMMENTS = Comment.ListarComentarios();
 
-            return LocalRedirect("~/Feed/Comentar");
-        }
+        //     return LocalRedirect("~/Feed/Comentar");
+        // }
         
     
     
