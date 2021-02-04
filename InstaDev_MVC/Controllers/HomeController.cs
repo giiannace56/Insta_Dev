d606c82bd70d6b819ca9f63c94897e139e0008f4
Eduardo <eduzika2903@hotmail.com>
2021-02-04 11:10:08 -0300

    Cadastro e Login (detalhe pra arrumar)n

--- InstaDev_MVC/Controllers/HomeController.cs
+++ InstaDev_MVC/Controllers/HomeController.cs
@@ -1,33 +1,54 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using InstaDev_MVC.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using InstaDev_MVC.Models;
 
 namespace InstaDev_MVC.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+    {   
+      public string Mensagem { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+
+        Usuario UsuarioModel = new Usuario();
+        public IActionResult Privacy()
         {
-            _logger = logger;
+            return View();
+        }
+
+        [Route("Logar")]
+        public IActionResult Logar(IFormCollection form)
+        {
+            List<string> csv = UsuarioModel.ReadAllLinesCSV("Database/Usuario.csv");
+
+            // Verificamos se as informações passadas existe na lista de string
+            var logado =
+            csv.Find(
+                x =>
+                x.Split(";")[2] == form["Email"] &&
+                x.Split(";")[3] == form["Senha"]
+            );
+
+
+            // Redirecionamos o usuário logado caso encontrado
+            if (logado != null)
+            {
+                HttpContext.Session.SetString("_Username", logado.Split(";")[1]);
+
+                return LocalRedirect("~/");
+            }
+
+           
+
+
+            Mensagem = "Dados incorretos, tente novamente...";
+            return LocalRedirect("~/Home");
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-
     }
 }
